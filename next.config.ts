82474d7445219123c3b372726d9fa299bc3183f4
JunHyeok <akdnjs0308@gmail.com>
2026-01-26 12:17:26 +0900

    :recycle: Refactor: 개발 환경에 대한 next.config.ts 설정

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
+const isDevelopment = process.env.NODE_ENV === 'development';
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
+        protocol: isDevelopment ? "http" : "https",
         hostname: process.env.BASE_HOSTNAME || "localhost",
+        port: isDevelopment ? "8080" : ""
       },
     ],
   },
@@ -13,7 +16,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.BASE_URL}/api/:path*`,
+        destination: `${process.env.BASE_URL || "http://localhost:8080"}/api/:path*`,
       },
     ];
   },
